81d290f4662b9bb45a028bbcd37d3a0d3ff932b9
lorebiondi3 <73020009+lorebiondi3@users.noreply.github.com>
2021-07-19 19:12:04 +0200

    Comments and change map() in mapValues() to obtain 'result'

--- page-rank.py
+++ page-rank.py
@@ -4,6 +4,13 @@ import re
 
 
 def map_title(line):
+    """
+        Function used to build the graph starting from the text file RDD. We search for a title tag (<title> * </title>)
+        in the 'line' and we return it along with all its neighbors (if any). The neighbors are surrounded by double
+        squared brackets.
+        From this function we obtain an RDD with the following elements' structure: (title, [n1,....,nk])
+        :param line: a line of the input file (RDD element)
+    """
     title = re.findall('<title>(.*?)</title>', line)
     if title is not None:
         links = re.findall("\\[\\[(.*?)\\]\\]", line)
@@ -11,77 +18,138 @@ def map_title(line):
 
 
 def compute_contribute(node):
+    """
+        Function used to compute the contributes that 'node' has to send to its neighbors
+        The returned RDD has the following structure: (title, contribute)
+        :param node: a node of the 'graph_ranked' RDD. It has the following structure: (title, ([n1,...,nk], rank))
+    """
+    # key
     title = node[0]
+    # value
     neighbors = node[1][0]
     rank = node[1][1]
+
     if len(neighbors) != 0:
-        contribute = rank / (len(neighbors))
-        contributes_list = ([contribute] * len(neighbors))
+        contribute = rank / (len(neighbors))  # Contribute for each neighbor
+        contributes_list = ([contribute] * len(neighbors))  # We create a list of contributes, one for each neighbor
+
+        # We append an additional entry related to the node that we are considering
+        # This is done in order to cop (in the subsequent phases) with nodes that are not pointed by anyone
         contributes_list.append(0)
         neighbors.append(title)
+
+        # If the contribute is None  ...........
         if contribute is not None:
+            # We use the zip() method to zip together the two RDD.
+            # The resulting RDD has the structure '(title, contribute)'
             contributes_neighbors = zip(neighbors, contributes_list)
             return contributes_neighbors
+
+    # If the node doesn't have any neighbor, we return only the entry related to the node itself
     else:
         return [(title, 0)]
 
 
 def compute_contribute_sum(value1, value2):
+    """
+        Function used to compute the sum of all the contributes received by a node
+        The returned RDD has the following structure: (title, contributes_sum)
+        The pages that are not pointed by anyone will be returned along with a 'contributes_sum' value equal to 0
+        :param value1: the first contribute
+        :param value2: the second contribute
+    """
+
+    # Both the values must be not None, otherwise we return only one of them
     if value1 is not None and value2 is not None:
         return value1 + value2
+
+    # These cases are possible only when a page of the dataset is not pointed by anyone. In this case we have a unique '(title, 0)' entry
     if value1 is None:
         return value2
     if value2 is None:
         return value1
 
+
 def compute_rank(x):
-    rank = (alpha / nodes_number) + (1 - alpha) * x[1]
-    return x[0], rank
+    """
+        Function used to compute the page rank value
+        The returned RDD has the following 'value' field's structure: ([n1,...,nk], rank)
+        :param x: value field of the 'joined' RDD. This parameter has the following structure: ([n1,...,nk], contributes_sum)
+    """
+    rank = (alpha / nodes_number) + (1 - alpha) * x[1]  # x[1] is the contributes_sum
+    return x[0], rank  # x[0] is the list of neighbors
+
 
 def sort_by_rank(node):
-    return -node[1]
-
-# def complete_graph(node):
-#     title = node[0]
-#     rank = node[1][0]
-#     neighbors = node[1][1]
-#     if neighbors is None:
-#         return title, ([], rank)
-#     else:
-#         return title, (neighbors, rank)
-
+    """
+        Function used to compute the sort the pages based on their rank value
+        :param node: node object in the form (title, rank)
+    """
+    return -node[1]  # We use the minus to sort in decreasing order
 
 
 if __name__ == "__main__":
+
+    # Read input parameters from command line
+    # Usage: <iterations> <alpha> <input> <output>
     if len(sys.argv) == 5:
         iterations = int(sys.argv[1])
         alpha = float(sys.argv[2])
         input = sys.argv[3]
         output = sys.argv[4]
+    # Default parameters
     else:
         iterations = 3
         alpha = 0.05
         input = "wiki-micro.txt"
         output = "page_rank_spark"
+
+    # We run the Spark application on a Yarn cluster
     sc = SparkContext("yarn", "pagerank")
+
+    # We create a text file RDD
     text = sc.textFile(input)
-    # graph = text.map(map_title).cache()
-    graph = text.map(map_title)
+
+    # We create the graph
+    # Caching applied to the graph RDD because we will read these data multiple times
+    graph = text.map(map_title).cache()
+
+    # We count the number of nodes in the graph
+    # We don't apply the count() method directly to the 'text' RDD
+    # because we want to be sure to count the actual number of nodes in the graph
     nodes_number = graph.count()
-    # graph_ranked = graph.map(lambda x: (x[0], (x[1], 1 / nodes_number)))
+
+    # We have to assign the initial rank to each node
+    # To do this, we exploit the mapValues() method because it operates on the value only, while map() operates on
+    # the entire record (both key and value).
+    # The result is an RDD in the form: (title, ([n1,...,nk], 1/nodes_number))
     graph_ranked = graph.mapValues(lambda x: (x, 1 / nodes_number))
-    print(graph_ranked.collect())
+
+    # Start the iterations for the PageRank computation
     for i in range(iterations):
+
+        # We distribute the contributes of each node across the network
+        # We use the flatMap() method ....................
         contributes = graph_ranked.flatMap(compute_contribute)
+
+        # For each node, we sum the received contributes
         contribute_sums = contributes.reduceByKey(compute_contribute_sum)
-        # print(contribute_sums.collect())
+
+        # We remove the pages that don't belong to the dataset (<title> tag not present) but are only pointed by other pages
+        # To do this we exploit the join() transformation between 'graph' and 'contribute_sums'. The first contains only the dataset's pages, while the latter contains also the pages that we want to remove
+        # The resulting RDD is in the form: (title, ([n1,...,nk], contributes_sum))
         joined = graph.join(contribute_sums)
+
+        # Computation of the page rank value and updating of the 'graph_ranked' RDD
+        # We use the mapValues() method because we are not interested in operating with the key, but only with the value
+        # The final RDD is in the form: (title, ([n1,...,nk], rank))
         graph_ranked = joined.mapValues(compute_rank)
-        # ranked contiene tutti i nodi, sia del dataset iniziale, sia quelli che comparivano solo come vicini di altri nodi.
-        # graph contiene solo i nodi del dataset iniziale.
-        # Faccio il join per recuperare la lista di vicini dal dataset iniziale
-        # joined = ranked.leftOuterJoin(graph)
-        # joined = ranked.join(graph)
-    result = graph_ranked.map(lambda x: (x[0], x[1][1]))
+
+    # To obtain '(title, PageRank)' tuples, we exploit the mapValues() method
+    result = graph_ranked.mapValues(lambda x: x[1])  # We are not interested in the neighbors list (x[0])
+
+    # Sorting of the nodes in decreasing order, considering the rank value
     result = result.sortBy(sort_by_rank)
+
+    # Write the elements of the 'result' RDD as text file
     result.saveAsTextFile(output)
